93bf8cf92d13eab50c5bfac42001da8a1d312f68
jonathan parisot <parisot.jonathan@gmail.com>
2017-04-07 17:25:37 +0200

    can't get it to work yet

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'autoprefixer-rails'
 
 gem "pundit"
 gem "simple_token_authentication"
+gem 'swagger-docs'
 
 group :development, :test do
   gem 'binding_of_caller'
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,18 @@
 class Api::V1::UsersController < Api::V1::BaseController
   acts_as_token_authentication_handler_for User, except: [ :index ]
 
+  swagger_controller :users, "User Management"
+
+  swagger_api :index do
+    summary 'Returns all posts'
+    notes 'Notes...'
+  end
+
+  swagger_api :show do
+    summary 'Returns all posts'
+    notes 'Notes...'
+  end
+
   def index
     @users = policy_scope(User.all)
     authorize @users
